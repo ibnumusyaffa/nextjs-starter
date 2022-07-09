d754bf6278d88f33bafcbde36842b45c5d2551db
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2022-07-09 10:18:14 +0700

    feat: add react query

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
